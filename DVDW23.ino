779477dbf4f85a3aedb6f3e95cd25ab48661c921
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-10-29 21:17:34 +0100

    added the hall sensor

--- DVDW23.ino
+++ DVDW23.ino
@@ -12,7 +12,7 @@ const int photo4 = A3;
 const int photo5 = A4;
 const int photo6 = A5;
 
-const int hall = A6;
+const int hallPin = 6; // D7 on the XIAO
 
 // live readings
 int photo1_value = 0;
@@ -22,10 +22,16 @@ int photo4_value = 0;
 int photo5_value = 0;
 int photo6_value = 0;
 
+// hall value and frequency variables
 int hall_value = 0;
+int magnetState = HIGH; // Initial state (no magnet detected)
+unsigned long previousMillis = 0; // Stores the time of the previous detection
+unsigned long interval = 1000; // Interval to calculate frequency (1 second)
+
 
 void setup() {
   Serial.begin(9600);
+  pinMode(hallPin, INPUT);
 }
 
 void loop() {
@@ -37,6 +43,8 @@ void loop() {
     photo4_value = analogRead(photo4);
     photo5_value = analogRead(photo5);
     photo5_value = analogRead(photo6);
+
+    hall_value = digitalRead(hallPin);
 
     // send data to serial monitor
     Serial.print("sensor1||");
@@ -54,6 +62,24 @@ void loop() {
 
     Serial.print("hall||");
       Serial.println(hall_value);
+
+    // process hall sensor data
+      if (hall_value == LOW && magnetState == HIGH) { // if a magnet is detected
+        unsigned long currentMillis = millis(); // Get current time
+        unsigned long timeElapsed = currentMillis - previousMillis; // Calculate time elapsed since last detection
+        previousMillis = currentMillis; // Store current time for next comparison
+        float frequency = 1000.0 / timeElapsed; // Calculate frequency (Hz)
+        
+        // send data to serial monitor (Hz)
+        Serial.print("frequency||");
+        Serial.println(frequency, 2); 
+      
+        magnetState = LOW; 
+
+      } else if (hall_value == HIGH && magnetState == LOW) {
+        // Magnet is not detected
+        magnetState = HIGH;
+      }
  
   // wait 10 milliseconds before the next loop (ADC needs time to convert)
   delay(10);
