70da4320e8d7844459ca7889710113be37344eee
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-11-09 17:31:37 +0100

    debugged

--- DVDW23.ino
+++ DVDW23.ino
@@ -1,7 +1,7 @@
 // This is the script for the DVDW23 project
 // 6 GL5516LDR photoresistors are connected to the analog pins of a Seeed Studio XIAO ESP32S3
-// The ESP32S3 reads and sends a value to the serial monitor every 10 milliseconds, in aa format that can be read by Protopie Connect
-// Experiment: Hall sensor to detect movement of the bike pedal
+// The ESP32S3 reads and sends a value to the serial monitor every 10 milliseconds, in a format that can be read by Protopie Connect (detecting of cups are shot)
+// The ESP32S3 also reads the magnegtic field using a HALL sensor and sends both the presence of a magnet and the frequency to protopie connect (detecting pedal speed)
 // by Bas Baccarne
 
 // pins
@@ -12,9 +12,9 @@ const int photo4 = A3;
 const int photo5 = A4;
 const int photo6 = A5;
 
-const int hallPin = 6; // D7 on the XIAO
+const int hallPin = D8;
 
-// live readings
+// variables for photoresistor readings
 int photo1_value = 0;
 int photo2_value = 0;
 int photo3_value = 0;
@@ -22,31 +22,36 @@ int photo4_value = 0;
 int photo5_value = 0;
 int photo6_value = 0;
 
-// hall value and frequency variables
+// variables for the hall sensors and frequency calculation
 int hall_value = 0;
-int magnetState = HIGH; // Initial state (no magnet detected)
-unsigned long previousMillis = 0; // Stores the time of the previous detection
-unsigned long interval = 1000; // Interval to calculate frequency (1 second)
-
+int magnetState = HIGH;
+unsigned long previousMillis = 0;
+unsigned long interval = 1000;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(hallPin, INPUT);
+  pinMode(hallPin, INPUT_PULLUP);
+  pinMode(photo1, INPUT);
+  pinMode(photo2, INPUT);
+  pinMode(photo3, INPUT);
+  pinMode(photo4, INPUT);
+  pinMode(photo5, INPUT);
+  pinMode(photo6, INPUT);
 }
 
 void loop() {
 
-  // read data 
+  // read data from all sensors
     photo1_value = analogRead(photo1);
     photo2_value = analogRead(photo2);
     photo3_value = analogRead(photo3);
     photo4_value = analogRead(photo4);
     photo5_value = analogRead(photo5);
-    photo5_value = analogRead(photo6);
+    photo6_value = analogRead(photo6);
 
     hall_value = digitalRead(hallPin);
 
-    // send data to serial monitor
+    // send raw data to serial monitor
     Serial.print("sensor1||");
       Serial.println(photo1_value);
     Serial.print("sensor2||");
@@ -54,16 +59,16 @@ void loop() {
     Serial.print("sensor3||");
       Serial.println(photo3_value);
     Serial.print("sensor4||");
-      Serial.println(photo4_value);
+     Serial.println(photo4_value);
     Serial.print("sensor5||");
-      Serial.println(photo5_value);
+     Serial.println(photo5_value);
     Serial.print("sensor6||");
-      Serial.println(photo6_value);  
+     Serial.println(photo6_value);  
 
     Serial.print("hall||");
-      Serial.println(hall_value);
+     Serial.println(hall_value);
 
-    // process hall sensor data
+    // process hall sensor data to calculate frequency
       if (hall_value == LOW && magnetState == HIGH) { // if a magnet is detected
         unsigned long currentMillis = millis(); // Get current time
         unsigned long timeElapsed = currentMillis - previousMillis; // Calculate time elapsed since last detection
